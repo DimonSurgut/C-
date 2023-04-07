4ea2814a9f80e0bcb3cbb3000afce24590c647c7
Dmitry <dimonsurgut1@mail.ru>
2023-04-07 15:34:21 +0300

    Завершил семинар

--- leson1/Program.cs
+++ leson1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// //Напишите программу,которая на входе 
+// принимает чмслом и выдает его квадрат (число умноженое само на себя)
+Console.WriteLine("Ведите целое число");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int square = number * number;
+Console.WriteLine ($"Квадрат число = {square}");
